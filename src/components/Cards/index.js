5b8be62b0338bfb20a9c9c8346c7a14d582ea625
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-11-12 15:40:44 -0300

    refactor(components): converting components to arrow functions
    convert all components from function component to arrow function component

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -3,29 +3,37 @@ import React, { useState } from "react";
 import * as S from "./index.style";
 import * as D from "../../data.json";
 
-export default function Cards() {
-    const [tabCards, setTabCards] = useState(0);
+export const Cards = () => {
+	const [tabCards, setTabCards] = useState(0);
 
-    return (
-        <>
-            <S.Tabs>
-                {D.content.map(({ title }, i) => {
-                    return (
-                        <S.Tab onClick={() => setTabCards(i)} key={i} active={tabCards === i ? true : false}>
-                            {title}
-                        </S.Tab>
-                    );
-                })}
-            </S.Tabs>
-            <S.Cards>
-                {D.content[tabCards].contents.map(({ background, link, title }, i) => {
-                    return (
-                        <S.Card style={{ backgroundImage: `url(${background})` }} href={link} key={i}>
-                            <S.Title>{title}</S.Title>
-                        </S.Card>
-                    );
-                })}
-            </S.Cards>
-        </>
-    );
-}
+	return (
+		<>
+			<S.Tabs>
+				{D.content.map(({ title }, i) => {
+					return (
+						<S.Tab
+							onClick={() => setTabCards(i)}
+							key={i}
+							active={tabCards === i ? true : false}
+						>
+							{title}
+						</S.Tab>
+					);
+				})}
+			</S.Tabs>
+			<S.Cards>
+				{D.content[tabCards].contents.map(({ background, link, title }, i) => {
+					return (
+						<S.Card
+							style={{ backgroundImage: `url(${background})` }}
+							href={link}
+							key={i}
+						>
+							<S.Title>{title}</S.Title>
+						</S.Card>
+					);
+				})}
+			</S.Cards>
+		</>
+	);
+};
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,25 +7,25 @@ import githubIcon from "../../assets/github.svg";
 import linkedinIcon from "../../assets/linkedin.svg";
 import emailIcon from "../../assets/gmail.svg";
 
-export default function Contact() {
-    return (
-        <S.Box>
-            <S.Contact>
-                <div>
-                    <a href={D.contact.github}>
-                        <img src={githubIcon} alt="Email" />
-                        <span>GitHub</span>
-                    </a>
-                    <a href={D.contact.linkedin}>
-                        <img src={linkedinIcon} alt="Email" />
-                        <span>Linkedin</span>
-                    </a>
-                    <a href={`mailto:${D.contact.email}`}>
-                        <img src={emailIcon} alt="Email" />
-                        <span>Email</span>
-                    </a>
-                </div>
-            </S.Contact>
-        </S.Box>
-    );
-}
+export const Contact = () => {
+	return (
+		<S.Box>
+			<S.Contact>
+				<div>
+					<a href={D.contact.github}>
+						<img src={githubIcon} alt="Email" />
+						<span>GitHub</span>
+					</a>
+					<a href={D.contact.linkedin}>
+						<img src={linkedinIcon} alt="Email" />
+						<span>Linkedin</span>
+					</a>
+					<a href={`mailto:${D.contact.email}`}>
+						<img src={emailIcon} alt="Email" />
+						<span>Email</span>
+					</a>
+				</div>
+			</S.Contact>
+		</S.Box>
+	);
+};
